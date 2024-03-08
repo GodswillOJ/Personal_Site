2d21d243ccf4ddee7fe41ad63885518123bedc5d
GodswillOJ <110397088+GodswillOJ@users.noreply.github.com>
2024-03-08 21:18:33 +0100

    hmvm

--- Backend/scripts.js
+++ Backend/scripts.js
@@ -36,11 +36,11 @@ db.once('open', () => {
 
 
 // Serve the React app for all 
-// app.use(express.static(path.join(__dirname, 'Client', 'build')));
+app.use(express.static(path.join(__dirname, 'Client', 'build')));
 
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'Client', 'build', 'index.html')); // Adjust path accordingly
-// });
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Client', 'build', 'index.html')); // Adjust path accordingly
+});
 app.use('/api', userRouter);
 
 app.listen(PORT, () => {
